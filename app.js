44bae865a0a0b175c05ac72fb0825bb1c8d28919
elmi-elmi <shahrokhelmi@gmail.com>
2021-10-11 20:29:37 +0330

    video 22

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 let vm = Vue.createApp({
-}).mount('#app');
+    data() {
+        return { isPurple: false, selectedColor: '' }
+    },
+    computed: {
+        circle_classes() {
+            return { purple: this.isPurple }
+        }
+    }
+
+}).mount('#app')
